9924660f02fd9a8732e2fa3924909d1def6c336b
oksanabolbat <oksana.bolbat@gmail.com>
2024-09-03 16:30:45 +0200

    fix: added exception for response

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ bg_image = tk.PhotoImage(file="images/background.png")
 bg_canvas = tk.Canvas(width=300, height=414)
 bg_canvas.create_image(150, 207, image=bg_image)
 
-
 quote = bg_canvas.create_text(150, 207, width=250, font=("Arial", 20, "bold"), text="text ...", fill="#fff")
 bg_canvas.grid(row=0, column=0)
 
+
 def generate_quote():
     resp = requests.get(url=URL)
+    resp.raise_for_status()
     quote_text = resp.json()["quote"]
     bg_canvas.itemconfig(quote, text=quote_text)
 
@@ -25,7 +26,5 @@ btn_quote = tk.Button()
 btn_image = tk.PhotoImage(file="images/kanye.png")
 btn_quote.config(image=btn_image, width=100, height=131, command=generate_quote)
 btn_quote.grid(row=1, column=0)
-
-
 
 window.mainloop()
